66445dd652e70aae3d176fd473cc4d31d214a658
paulseghers <0099seghers@gmail.com>
2023-03-01 18:22:46 +0100

    refactor dummy data out of fake api server

--- fake_dependencies/selfservice-api/src/index.js
+++ fake_dependencies/selfservice-api/src/index.js
@@ -1,101 +1,13 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import { kafkaClusters, capabilities } from "./dummyData";
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
 const port = process.env.PORT || 3001;
-
-const kafkaClusters = [
-  {
-    id: "kc-1",
-    name: "Development",
-    description: "This cluster is for non-production workloads and used in various staging environments. Please note, you should not consume messages in this cluster from your production workloads!",
-  },
-  {
-    id: "kc-2",
-    name: "Production",
-    description: "This is the production cluster used for production workloads. This cluster has the highes resource allocations and is used for business critical messages.",
-  }
-];
-
-
-const capabilities = [
-    {
-      id: "this-is-a-capability-xyz",
-      name: "this is a capability",
-      description: "lksd lskd flskdnf lskerntolweirhtn lis dflk slkdmf",
-      members: [
-        {
-          email: "jandr@dfds.com"
-        },
-        {
-          email: "thfis@dfds.com"
-        },
-      ],
-      topics: [
-        {
-          id: "1",
-          name: "this-is-a-capability-xyz.foo",
-          description: "this is a foo topic for foo stuff",
-          kafkaClusterId: "kc-2",
-          partitions: 3,
-          retention: 1,
-          status: "Provisioned"
-        },
-        {
-          id: "2",
-          name: "pub.this-is-a-capability-xyz.bar",
-          description: "a public topic for bar stuff",
-          kafkaClusterId: "kc-2",
-          partitions: 3,
-          retention: 1,
-          status: "Provisioned"
-        },
-        {
-          id: "3",
-          name: "this-is-a-capability-xyz.a-foo",
-          description: "a topic for a-foo stuff",
-          kafkaClusterId: "kc-2",
-          partitions: 3,
-          retention: 1,
-          status: "Provisioned"
-        },
-        {
-          id: "4",
-          name: "this-is-a-capability-xyz.b-foo",
-          description: "a topic for b-foo stuff",
-          kafkaClusterId: "kc-2",
-          partitions: 3,
-          retention: 1,
-          status: "In Progress"
-        }
-      ]
-    },
-    {
-      id: "another-awssome-capability-abcd",
-      name: "another awssome capability",
-      description: "lknm lk23lnk nl kl23lk lk",
-      members: [
-        {
-          email: "thfis@dfds.com"
-        },
-      ],
-      topics: [
-        {
-          id: "3",
-          name: "another-awssome-capability-abcd.name-of-topic",
-          description: "just a dummy topic",
-          kafkaClusterId: "kc-2",
-          partitions: 3,
-          retention: 1,
-          status: "Provisioned"
-        }
-      ]
-    },
-];
 
 // ----------------------------------------------------------------------------------------------------
 
