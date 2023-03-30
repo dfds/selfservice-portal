2705115b733fafa9b8a3adca8dd676c5b42e1811
Jens Andresen <jandr@DFDS.COM>
2023-03-30 11:59:43 +0200

    add column for approve buttons in membership application component

--- src/src/pages/capabilities/membershipapplications/index.js
+++ src/src/pages/capabilities/membershipapplications/index.js
@@ -1,43 +1,43 @@
-import { Text } from '@dfds-ui/typography';
 import { Button, Table, TableHead, TableBody, TableRow, TableHeaderCell, TableDataCell } from '@dfds-ui/react-components'
 import { useContext } from "react";
 import PageSection from "components/PageSection";
 import AppContext from "app-context";
-import { format, intlFormatDistance,} from "date-fns";
+import { format, intlFormatDistance, } from "date-fns";
 
-export function MembershipApplications(id, userEmail){
+export function MembershipApplications(id, userEmail) {
     const { selectedCapability } = useContext(AppContext);
-    // only display membership applications banner if there are membership applications
-    let nonEmpty = false;
-    if (selectedCapability.membershipApplications.length > 0){
-        nonEmpty = true;
-    }
-   return <>
-    {nonEmpty && <Text styledAs='sectionHeadline'>Membership applications</Text>}
-    {nonEmpty && <PageSection>
-    <Table isInteractive width={"100%"}>
-        <TableHead>
-                <TableRow>
-                    <TableHeaderCell>Applicant</TableHeaderCell>
-                    <TableHeaderCell>Application date</TableHeaderCell>
-                    <TableHeaderCell>Expires</TableHeaderCell>
-                    <TableHeaderCell>status</TableHeaderCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-            {selectedCapability.membershipApplications.map(x =>
-            <TableRow key={x.applicant}>
-                <TableDataCell>{x.applicant}</TableDataCell>
-                <TableDataCell>{format(new Date(x.submittedAt), 'MMMM do yyyy, h:mm:ss')}</TableDataCell>
-                <TableDataCell>{intlFormatDistance(new Date(x.expiresOn), new Date())}</TableDataCell>
-                <TableDataCell>{x.status}</TableDataCell>
-                { selectedCapability.userCanApprove && <TableDataCell> <Button size='small' >Approve</Button></TableDataCell> }
-                </TableRow>
-            )}
-        </TableBody>
-    </Table>
-</PageSection>}
-</>
+    const hasPendingApplications = selectedCapability.membershipApplications.length > 0;
+
+    return <>
+        { hasPendingApplications && 
+            <PageSection headline="Membership Applications">
+                <Table isInteractive width={"100%"}>
+                    <TableHead>
+                        <TableRow>
+                            <TableHeaderCell>Applicant</TableHeaderCell>
+                            <TableHeaderCell>Application date</TableHeaderCell>
+                            <TableHeaderCell>Expires</TableHeaderCell>
+                            <TableHeaderCell>Status</TableHeaderCell>
+                            <TableHeaderCell></TableHeaderCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {selectedCapability.membershipApplications.map(x =>
+                            <TableRow key={x.applicant}>
+                                <TableDataCell>{x.applicant}</TableDataCell>
+                                <TableDataCell>{format(new Date(x.submittedAt), 'MMMM do yyyy, h:mm:ss')}</TableDataCell>
+                                <TableDataCell>{intlFormatDistance(new Date(x.expiresOn), new Date())}</TableDataCell>
+                                <TableDataCell>{x.status}</TableDataCell>
+                                <TableDataCell align='right'>
+                                    <Button size='small' >Approve</Button>
+                                </TableDataCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </PageSection>
+        }
+    </>
 }
 
 export default MembershipApplications;
