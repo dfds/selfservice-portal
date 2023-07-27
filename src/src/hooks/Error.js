f4f0f43d951a8055836c6b8d8286e7fe1b80f75d
Emil H. Clausen <sequoiia@hummel.yt>
2023-07-27 15:00:10 +0200

    Rearranged triggerError function, added comments

--- src/src/hooks/Error.js
+++ src/src/hooks/Error.js
@@ -2,47 +2,62 @@ import { useContext, useState } from "react";
 import ErrorContext from "ErrorContext";
 
 export function useError(opts) {
-  const [options, setOptions] = useState(opts);
-  const { showError } =  useContext(ErrorContext);
+    const [options, setOptions] = useState(opts);
+    const { showError } = useContext(ErrorContext);
 
-  const triggerError = (params) => {
-      if (options) {
-          if (options.handler) {
-              params.showError = showError;
-              if (params.msg === null) {
-                  params.msg = options.msg;
-              }
-              options.handler(params);
-          } else {
-              if (params) {
-                  params.showError = showError;
+    ///
+    /// triggerError
+    ///
+    /// Priority of error handling flow
+    ///   1: Handler provided via params
+    ///   2: Handler provided via options
+    ///   3: Error provided via params
+    ///   4: Msg provided via params
+    ///   5: fallbackMsg
+    ///
+    const triggerError = (params) => {
+        // Setup
+        let fallbackMsg = "error";
+        if (params) {
+            params.showError = showError;
+        }
 
-                  if (params.handler) {
-                      params.handler(params);
-                      return;
-                  }
+        if (options?.msg != null) {
+            fallbackMsg = options.msg; // If a catch-all error message is available in options, make sure it is used as a fallback
+        }
 
-                  if (params.error) {
-                      showError(params.error.message);
-                      return;
-                  }
+        // Error handling flow
+        if (params?.handler != null) { // 1: Handler provided via params
+            params.handler(params);
+            return;
+        }
 
-                  if (params.msg) {
-                      showError(params.msg);
-                      return;
-                  }
+        if (options?.handler != null) { // 2: Handler provided via options
+            if (params.msg === null) {
+                params.msg = fallbackMsg;
+            }
+            options.handler(params);
+            return;
+        }
 
-                  showError(options.msg);
-              } else {
-                  showError(options.msg);
-              }
-          }
-      }
-  }
+        if (params) {
+            if (params.error) { // 3: Error provided via params
+                showError(params.error.message);
+                return;
+            }
 
-  return {
-    options,
-    setErrorOptions: setOptions,
-    triggerError
-  }
+            if (params.msg) { // 4: Msg provided via params
+                showError(params.msg);
+                return;
+            }
+        }
+
+        showError(fallbackMsg); // 5: fallbackMsg
+    }
+
+    return {
+        options,
+        setErrorOptions: setOptions,
+        triggerError
+    }
 }
