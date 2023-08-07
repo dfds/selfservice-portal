23a5381cbd33f238f6ed6039af7ba6b3edf74fd2
Kristian Pilegard Jensen <kralle@gmail.com>
2023-08-07 09:56:17 +0200

    Made custom labels work again

--- src/src/components/BasicCapabilityCost/index.js
+++ src/src/components/BasicCapabilityCost/index.js
@@ -11,7 +11,7 @@ export function CapabilityCostSummary({data}) {
                     <XAxis dataKey="timestamp" hide/>
                     <YAxis type="number" domain={['dataMin', 'dataMax']} hide>
                     </YAxis>
-                    <Tooltip/>
+                    <Tooltip content={CostTooltip}/>
                     <CartesianGrid/>
                     <Line type="monotone" dataKey="pv" stroke="#014874" strokeWidth={2} dot={false}
                           isAnimationActive={false}/>
@@ -28,7 +28,7 @@ export function LargeCapabilityCostSummary({data}) {
                 <LineChart data={data}>
                     <XAxis hide/>
                     <YAxis type="number" domain={[dataMin => (0), dataMax => (dataMax * 1.2)]} />/>
-                    <Tooltip/>
+                    <Tooltip content={CostTooltip}/>
                     <CartesianGrid strokeDasharray="1 1"/>
                     <Line type="monotone" dataKey="pv" stroke="#055874" strokeWidth={2} dot={false}
                           isAnimationActive={false}/>
@@ -44,12 +44,13 @@ const CostTooltip = ({active, payload, label}) => {
             <div className={styles.customTooltip}>
                 <p className="label"><span
                     className={styles.tooltipName}>{new Date(Date.parse(payload[0].payload.name)).toLocaleDateString()}:</span>
-                    <span className={styles.tooltipValue}>{payload[0].value.toFixed(2)}$</span></p>
+                    <span className={styles.tooltipValue}>{payload[0].value}$</span></p>
             </div>
         );
     }
 
     return null;
 };
+
 
 export default CapabilityCostSummary;
