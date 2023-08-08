fc574943a7decbf85afb148b0e1a58b7aa47207b
Kristian Pilegard Jensen <kralle@gmail.com>
2023-08-08 14:41:20 +0200

    fixed broken file after merge

--- src/src/pages/capabilities/MyCapabilities.js
+++ src/src/pages/capabilities/MyCapabilities.js
@@ -1,74 +1,108 @@
-import React, {useContext, useEffect, useState} from "react";
-import {Text} from '@dfds-ui/typography';
-import {useNavigate} from "react-router-dom";
-import {ChevronRight, StatusAlert} from '@dfds-ui/icons/system';
-import {Table, TableHead, TableBody, TableRow, TableHeaderCell, TableDataCell} from '@dfds-ui/react-components'
-import {Spinner} from '@dfds-ui/react-components';
+import React, { useContext, useEffect, useState } from "react";
+import { Text } from "@dfds-ui/typography";
+import { useNavigate } from "react-router-dom";
+import { ChevronRight, StatusAlert } from "@dfds-ui/icons/system";
+import {
+  Spinner,
+  Table,
+  TableBody,
+  TableDataCell,
+  TableHead,
+  TableHeaderCell,
+  TableRow,
+} from "@dfds-ui/react-components";
 import AppContext from "AppContext";
 import PageSection from "components/PageSection";
-import CapabilityCostSummary from 'components/BasicCapabilityCost';
+import CapabilityCostSummary from "components/BasicCapabilityCost";
 
 export default function MyCapabilities() {
-    const {myCapabilities, capabilityCosts, appStatus} = useContext(AppContext);
+  const { myCapabilities, capabilityCosts, appStatus } = useContext(AppContext);
 
-    const items = myCapabilities || [];
-    const isLoading = !appStatus.hasLoadedMyCapabilities;
-    const isLoadingCosts = !appStatus.hasLoadedCosts;
-    const [showCostsSpinner, setShowCostsSpinner] = useState(isLoadingCosts);
+  const items = myCapabilities || [];
+  const isLoading = !appStatus.hasLoadedMyCapabilities;
+  const isLoadingCosts = !appStatus.hasLoadedCosts;
+  const [showCostsSpinner, setShowCostsSpinner] = useState(isLoadingCosts);
 
-    const navigate = useNavigate();
-    const clickHandler = (id) => navigate(`/capabilities/${id}`);
+  const navigate = useNavigate();
+  const clickHandler = (id) => navigate(`/capabilities/${id}`);
 
-    useEffect(() => {
-        // Simulate fetching data from an API
-        setTimeout(() => {
-            setShowCostsSpinner(false);
-        }, 3000);
-    }, []);
+  useEffect(() => {
+    // Simulate fetching data from an API
+    setTimeout(() => {
+      setShowCostsSpinner(false);
+    }, 3000);
+  }, []);
 
-    useEffect(() => {
-        setShowCostsSpinner(isLoadingCosts);
-    }, [isLoadingCosts]);
+  useEffect(() => {
+    setShowCostsSpinner(isLoadingCosts);
+  }, [isLoadingCosts]);
 
-    return <>
-        <PageSection headline={`My Capabilities ${isLoading ? "" : `(${items.length})`}`}>
-            {isLoading && <Spinner/>}
+  return (
+    <>
+      <PageSection
+        headline={`My Capabilities ${isLoading ? "" : `(${items.length})`}`}
+      >
+        {isLoading && <Spinner />}
 
-            {!isLoading && items.length === 0 &&
-                <Text>Oh no! You have not joined a capability...yet! Knock yourself out with the ones below...</Text>}
+        {!isLoading && items.length === 0 && (
+          <Text>
+            Oh no! You have not joined a capability...yet! Knock yourself out
+            with the ones below...
+          </Text>
+        )}
 
-            {!isLoading && items.length > 0 && <>
+        {!isLoading && items.length > 0 && (
+          <>
             <Table isInteractive width={"100%"}>
-                <TableHead>
-                    <TableRow>
-                        <TableHeaderCell>Name</TableHeaderCell>
-                        <TableHeaderCell align="center">Costs</TableHeaderCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {items.map(x => <TableRow key={x.id} onClick={() => clickHandler(x.id)}>
-                        <TableDataCell>
-                            <Text className="warningIcon" hidden={x.status == "Active"}><StatusAlert/></Text>
-                            <Text styledAs="action" as={"div"}>{x.name}</Text>
-                            <Text styledAs="caption" as={"div"}>{x.description}</Text>
-                        </TableDataCell>
-                        <TableDataCell align="center" width="100px">
-                            {
-                                showCostsSpinner ? <Spinner/> : isLoadingCosts ? (
-                                    <Text styledAs="caption" as={"div"}>No data available</Text>
-                                ) : <>
-                                    <CapabilityCostSummary
-                                        data={capabilityCosts.getCostsForCapability(x.id, 7)}/>
-                                    <ChevronRight/>
-                                </>
-                            }
-
-                        </TableDataCell>
-                    </TableRow>)}
-                </TableRow>)}
-            </TableBody>
+              <TableHead>
+                <TableRow>
+                  <TableHeaderCell>Name</TableHeaderCell>
+                  <TableHeaderCell align="center">Costs</TableHeaderCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {items.map((x) => (
+                  <TableRow key={x.id} onClick={() => clickHandler(x.id)}>
+                    <TableDataCell>
+                      <Text
+                        className="warningIcon"
+                        hidden={x.status == "Active"}
+                      >
+                        <StatusAlert />
+                      </Text>
+                      <Text styledAs="action" as={"div"}>
+                        {x.name}
+                      </Text>
+                      <Text styledAs="caption" as={"div"}>
+                        {x.description}
+                      </Text>
+                    </TableDataCell>
+                    <TableDataCell align="center" width="100px">
+                      {showCostsSpinner ? (
+                        <Spinner />
+                      ) : isLoadingCosts ? (
+                        <Text styledAs="caption" as={"div"}>
+                          No data available
+                        </Text>
+                      ) : (
+                        <>
+                          <CapabilityCostSummary
+                            data={capabilityCosts.getCostsForCapability(
+                              x.id,
+                              7,
+                            )}
+                          />
+                          <ChevronRight />
+                        </>
+                      )}
+                    </TableDataCell>
+                  </TableRow>
+                ))}
+              </TableBody>
             </Table>
-                </>}
-        </PageSection>
+          </>
+        )}
+      </PageSection>
     </>
+  );
 }
