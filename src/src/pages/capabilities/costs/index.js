eb8f016ac7608814a5000d847bdd7614e1bf222b
Kristian Pilegaard Jensen <kralle@gmail.com>
2023-08-15 14:24:06 +0200

    Removed actual finout link. Added text looking like link that says coming soon.

--- src/src/pages/capabilities/costs/index.js
+++ src/src/pages/capabilities/costs/index.js
@@ -6,6 +6,7 @@ import styles from "../summary/summary.module.css";
 import { Text } from "@dfds-ui/typography";
 import { LargeCapabilityCostSummary } from "../../../components/BasicCapabilityCost";
 import { Spinner } from "@dfds-ui/react-components";
+import costsStyles from "./costs.module.css";
 
 export default function Costs() {
   const { appStatus, capabilityCosts } = useContext(AppContext);
@@ -53,12 +54,9 @@ export default function Costs() {
                         data={dataValue}
                         capabilityId={id}
                       />
-                      <a
-                        target="_blank"
-                        href={`https://app.finout.io/app/total-cost?accountId=e071c3ed-1e3c-46f7-9830-71951712d791&context=%7B%22id%22%3A%2288dc362c-5876-45d9-8c9e-950f1f481e78%22%2C%22metricName%22%3A%22MegaBill%22%2C%22type%22%3A%22cost%22%2C%22name%22%3A%22MegaBill%22%2C%22label%22%3A%22unlabeled%22%2C%22tags%22%3A%7B%7D%2C%22costViewId%22%3A%2232%22%2C%22unitAggregationCount%22%3A1%7D&gbyHiddenLegendIndexes=0_2&filters=%7B%22costCenter%22%3A%22virtualTag%22%2C%22key%22%3A%2252c02d7e-093a-42b7-bf06-eb13050a8687%22%2C%22path%22%3A%22Virtual+Tags%F0%9F%94%A5%2Fcapability%22%2C%22type%22%3A%22virtual_tag%22%2C%22operator%22%3A%22is%22%2C%22value%22%3A%22${id}%22%${days}D`}
-                      >
-                        Finout link
-                      </a>
+                      <Text className={costsStyles.finout_link} >
+                        <ins>Finout link coming soon</ins>
+                      </Text>
                     </>
                   )}
                 </>
