5196ec0fd39cdb7f567395e96de377e7742e0820
Emil H. Clausen <sequoiia@hummel.yt>
2023-08-17 13:40:38 +0200

    Don't show img element if user doesn't have a profile picture

--- src/src/pages/capabilities/members/profilepicture.js
+++ src/src/pages/capabilities/members/profilepicture.js
@@ -2,10 +2,11 @@ import React from "react";
 import styles from "./profilepicture.module.css";
 
 export default function ProfilePicture({ name, pictureUrl }) {
+  console.log(pictureUrl);
   return (
     <div className={styles.container}>
       <div className={styles.background} title={name}>
-        <img src={pictureUrl} alt="profile" />
+        {(pictureUrl != "" && pictureUrl != undefined) && <img src={pictureUrl} alt="profile" />}
       </div>
       <div className={styles.name}>{name}</div>
     </div>
