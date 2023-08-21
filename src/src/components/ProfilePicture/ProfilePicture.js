2dec054cc114478d83f1e922246d6c1a53b9ee00
Emil H. Clausen <sequoiia@hummel.yt>
2023-08-21 15:46:12 +0200

    Fixed conditional logic for profile picture

--- src/src/components/ProfilePicture/ProfilePicture.js
+++ src/src/components/ProfilePicture/ProfilePicture.js
@@ -3,8 +3,7 @@ import styles from "./ProfilePicture.module.css";
 import { Account } from "@dfds-ui/icons/system";
 
 export default function ProfilePicture({ name, pictureUrl }) {
-  const hasPicture =
-    pictureUrl !== undefined && pictureUrl != null && pictureUrl !== "";
+  const hasPicture = pictureUrl != "" && pictureUrl != undefined;
   return (
     <div className={styles.container}>
       <div className={styles.background} title={name}>
