a2a21683e9aa34664af8c33b5bf3829c1ed56783
Emil H. Clausen <sequoiia@hummel.yt>
2023-08-22 14:45:53 +0200

    Removed unnecessary log

--- src/src/TrackingContext.js
+++ src/src/TrackingContext.js
@@ -10,7 +10,6 @@ function TrackingProvider({ children }) {
 
   useEffect(() => {
     if (isEnabled) {
-      console.log("Tracking enabled");
       var _mtm = window._mtm = window._mtm || [];
       _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
       var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
