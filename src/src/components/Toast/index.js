6f8be034d184d3dafa70089dda160846a8b69aaf
Kristian Pilegaard Jensen <kralle@gmail.com>
2023-09-05 10:17:04 +0200

    restructured css to show details button

--- src/src/components/Toast/index.js
+++ src/src/components/Toast/index.js
@@ -1,6 +1,7 @@
 import styles from "./toast.module.css";
 import { Close } from "@dfds-ui/icons/system";
 import { useState } from "react";
+import { Button } from "@dfds-ui/react-components";
 
 export default function Toast({ message }) {
   const [isOpen, setIsOpen] = useState(true);
@@ -10,12 +11,18 @@ export default function Toast({ message }) {
 
   return (
     <div className={styles.container_toast}>
-      <div className={styles.textbox}>{message}</div>
-      <div className={styles.actionbox}>
-        <div className={styles.toast_close} onClick={() => setIsOpen(false)}>
-          <Close />
+      <div className={styles.container_toast_message}>
+        <div className={styles.textbox}>{message}</div>
+        <div className={styles.actionbox}>
+          <div onClick={() => setIsOpen(false)}>
+            <Close />
+          </div>
         </div>
-        {/* <Button size="small" variation="outlined">Details</Button> */}
+      </div>
+      <div>
+        <Button size="small" variation="link" fillWidth="true">
+          Details
+        </Button>
       </div>
     </div>
   );
