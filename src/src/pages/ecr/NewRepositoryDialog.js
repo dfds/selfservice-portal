4e99437176f2308cc842956660842b1b75f1edb4
Andreas Frisch <andfris@dfds.com>
2023-09-13 10:18:36 +0200

    Hooking into real-er backend for ECR repositories

--- src/src/pages/ecr/NewRepositoryDialog.js
+++ src/src/pages/ecr/NewRepositoryDialog.js
@@ -2,17 +2,13 @@ import React, { useState, useContext } from "react";
 import { Button, ButtonStack } from "@dfds-ui/react-components";
 import { SideSheet, SideSheetContent } from "@dfds-ui/react-components";
 import { TextField } from "@dfds-ui/react-components";
-import ECRContext from "./ECRContext";
+import { useECRRepositories } from "hooks/ECRRepositories";
 
 export default function NewRepositoryDialog({
   onClose
 }) {
-  const {
-    addRepository,
-    setIsCreatingNewRepository,
-    setPollForUpdates,
-    isCreatingNewRepository
-  } = useContext(ECRContext);
+  const [isCreatingNewRepository, setIsCreatingNewRepository] = useState(false);
+  const { createRepository } = useECRRepositories();
 
   const handleClose = () => {
     if (!isCreatingNewRepository) {
@@ -20,22 +16,29 @@ export default function NewRepositoryDialog({
     }
   };
 
-  //error banner logic
-  //const displayConflictWarning =
-
   const emptyValues = {
-    name: "",
+    //name: "",
     description: "",
+    repositoryName: "",
   };
 
   const [formData, setFormData] = useState(emptyValues);
 
+  /*
   const changeName = (e) => {
     e.preventDefault();
-    let newName = e?.target?.value || "";
+    let newName = e?.target?.value || emptyValues.name;
+    
+    setFormData((prev) => ({ ...prev, ...{ name: newName.toLowerCase() } }));
+  };
+  */
+
+  const changeRepositoryName = (e) => {
+    e.preventDefault();
+    let newName = e?.target?.value || emptyValues.repositoryName;
     newName = newName.replace(/\s+/g, "/");
 
-    setFormData((prev) => ({ ...prev, ...{ name: newName.toLowerCase() } }));
+    setFormData((prev) => ({ ...prev, ...{ repositoryName: newName.toLowerCase() } }));
   };
 
   const changeDescription = (e) => {
@@ -43,7 +46,7 @@ export default function NewRepositoryDialog({
     const newValue = e?.target?.value || emptyValues.description;
     setFormData((prev) => ({ ...prev, ...{ description: newValue } }));
   };
-
+  /*
   const isNameValid =
     formData.name !== "" &&
     !formData.name.match(/^\s*$/g) &&
@@ -52,24 +55,41 @@ export default function NewRepositoryDialog({
     !formData.name.match(/[-_\/\.]{2,}/g) &&
     !formData.name.match(/[^a-zA-Z0-9\/\-_]/g);
 
-  let nameErrorMessage = "";
+    let nameErrorMessage = "";
   if (formData.name.length > 0 && !isNameValid) {
     nameErrorMessage =
-      'Allowed characters are a-z, 0-9, "-", "_", and "/". Names must not start or end with "_", "-", or "/". Do not use more than one of "-", "_", or "/" in a row.';
+      'Allowed characters are a-z, 0-9, "-", "_", and " ". Names must not start or end with "_", "-", or "/". Do not use more than one of "-", "_", or "/" in a row.';
   }
-  if (formData.name.length > 150) {
-    nameErrorMessage = "Please consider a shorter name.";
+
+  */
+  const isRepositoryNameValid =
+    formData.repositoryName !== "" &&
+    !formData.repositoryName.match(/^\s*$/g) &&
+    !formData.repositoryName.match(/(_|-|\/)$/g) &&
+    !formData.repositoryName.match(/^(_|-|\/)/g) &&
+    !formData.repositoryName.match(/[-_\/\.]{2,}/g) &&
+    !formData.repositoryName.match(/[^a-zA-Z0-9\/\-_]/g);
+  
+  let repositoryNameErrorMessage = "";
+  if (formData.repositoryName.length > 0 && !isRepositoryNameValid) {
+    repositoryNameErrorMessage =
+      'Allowed characters are a-z, 0-9, "-", "_", and "/". Repositories must not start or end with "_", "-", or "/". Do not use more than one of "-", "_", or "/" in a row.';
+  }
+  
+  if (formData.repositoryName.length > 150) {
+    repositoryNameErrorMessage = "Please consider a shorter name.";
   }
 
   const canAdd =
-    formData.name !== "" &&
+    //formData.name !== "" &&
     formData.description !== "" &&
-    nameErrorMessage === "";
+    formData.repositoryName !== "" &&
+    //nameErrorMessage === "" &&
+    repositoryNameErrorMessage === "";
 
   const handleAddRepositoryClicked = async () => {
     setIsCreatingNewRepository(true);
-    await addRepository(formData);
-    setPollForUpdates(true);
+    await createRepository(formData);
     setIsCreatingNewRepository(false);
     onClose();
   };
@@ -86,6 +106,7 @@ export default function NewRepositoryDialog({
         backdrop
       >
         <SideSheetContent>
+          {/*
           <div>
             <TextField
               label="Name"
@@ -97,6 +118,15 @@ export default function NewRepositoryDialog({
               maxLength={255}
             />
           </div>
+          */}
+          <TextField
+            label="Repository Name"
+            placeholder="Enter a name for the new repository"
+            required
+            value={formData.repositoryName}
+            onChange={changeRepositoryName}
+            errorMessage={repositoryNameErrorMessage}
+          ></TextField>
 
           <TextField
             label="Description"
--- src/src/pages/ecr/index.js
+++ src/src/pages/ecr/index.js
@@ -1,86 +1,92 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useMemo } from "react";
+import { MaterialReactTable } from 'material-react-table';
 import {
   Card,
   CardTitle,
   CardContent,
   CardActions,
   Button,
-  Table,
-  TableBody,
-  TableDataCell,
-  TableHead,
-  TableHeaderCell,
-  TableRow,
   Text,
   Spinner, 
 } from "@dfds-ui/react-components"; 
 import Page from "components/Page";
 import PageSection from "components/PageSection";
-import ECRContext from "./ECRContext";
-import AppContext from "AppContext";
-import { ECRProvider } from "./ECRContext";
 import NewRepositoryDialog from "./NewRepositoryDialog";
-import styles from "./ecr.module.css";
+import { useECRRepositories } from "hooks/ECRRepositories";
 
-
-function Repositories() {
-  const { repositories } = useContext(ECRContext);
-  const { truncateString } = useContext(AppContext);
+function Repositories() {  
+  const { repositories } = useECRRepositories();
 
   useEffect(() => {
     console.log("Repositories updated: ", repositories.length);
   }, [repositories]);
 
-  /*
-  {repositories.map((repository) => (
-    <Card key={repository.id}>{repository.id} [{repository.status}] -- {repository.name}</Card>
-  ), repositories)}
-*/
-  const rowClass = (status) => status === "pending" ? styles.pendingRow : '';
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'repositoryName',
+        header: 'Name',
+        size: 250,
+        Cell: ({ renderedCellValue }) => {
+          return (
+            <Text styledAs="action" as={"div"}>
+                {renderedCellValue}
+            </Text>
+          )
+        }
+      },
+      {
+        accessorKey: 'description',
+        header: 'Description',
+        size: 250,
+        Cell: ({ renderedCellValue }) => {
+          return (
+            <Text styledAs="action" as={"div"}>
+                {renderedCellValue}
+            </Text>
+          )
+        }
+      }
+    ],
+    [],
+  )
+
 
   return (
     <>
       <PageSection headline={`Repositories`}>
       {((repositories || []).length === 0) && (<Spinner />)}
       {(repositories.length > 0) && (
-          <>
-            <Table isInteractive width={"100%"}>
-              <TableHead>
-                <TableRow>
-                  <TableHeaderCell>Name</TableHeaderCell>
-                  <TableHeaderCell align="center">URI</TableHeaderCell>
-                  <TableHeaderCell align="center">Note</TableHeaderCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {repositories.map((repository) => (
-                  <TableRow key={repository.id} className={rowClass(repository.status)}>
-                    <TableDataCell>
-                      <Text styledAs="action" as={"div"}>
-                        {truncateString(repository.name)}
-                      </Text>
-                      <Text styledAs="caption" as={"div"}>
-                        {truncateString(repository.description)}
-                      </Text>
-                    </TableDataCell>
-                    <TableDataCell>
-                        <Text styledAs="action" as={"div"}>
-                            {repository.uri}
-                        </Text>
-                    </TableDataCell>
-                    <TableDataCell>
-                      {(repository.status === "pending") && (
-                        <Text styledAs="caption" as={"div"}>
-                          Pending creation
-                        </Text>
-                      )}
-                    </TableDataCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </>
-        )}
+        <MaterialReactTable columns={columns} data={repositories}
+          muiTableHeadCellProps={{
+            sx: {
+              fontWeight: '700',
+              fontSize: '16px',
+              fontFamily: 'DFDS',
+              color: '#002b45',
+            },
+          }}
+          muiTableBodyCellProps={{
+            sx: {
+              fontWeight: '400',
+              fontSize: '16px',
+              fontFamily: 'DFDS',
+              color: '#4d4e4c',
+              padding: '5px',
+            },
+          }}
+          muiTablePaperProps={{
+            elevation: 0,
+            sx: {
+              borderRadius: '0',
+            }
+          }}
+          enableTopToolbar={true}
+          enableBottomToolbar={false}
+          enableColumnActions={true}
+          enableColumnFilters={true}
+       />
+      )}
       </PageSection>
     </>
   );
@@ -92,8 +98,7 @@ export default function ECRPage() {
 
   return (
     <>
-      <ECRProvider>
-        <Page title="ECR Repositories">
+      <Page title="ECR Repositories">
           {showNewRepositoryDialog && (<NewRepositoryDialog
             onClose={() => setShowNewRepositoryDialog(false)}
           />)}
@@ -122,7 +127,6 @@ export default function ECRPage() {
           </Card>
           <Repositories />
         </Page>
-      </ECRProvider>
     </>
   );
 }
