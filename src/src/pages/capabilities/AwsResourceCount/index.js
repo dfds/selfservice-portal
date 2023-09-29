c879557ff19507dae0714229bf966e6dec88b508
Andreas Frisch <andfris@dfds.com>
2023-09-29 08:36:19 +0200

    More specific AWS count UI

--- src/src/pages/capabilities/AwsResourceCount/index.js
+++ src/src/pages/capabilities/AwsResourceCount/index.js
@@ -29,13 +29,24 @@ export function DetailedAwsCountSummary({ capabilityId }) {
   const [showResourcesWindow, setShowResourcesWindow] = useState(false);
   const count = metricsWrapper.getAwsResourcesTotalCountForCapability(capabilityId);
   
-  const interests = ["rds", "s3", "ssm", "sqs", "ssn", "lambda", "ecs", "ec2", "iam"];
+  const interests = [
+    ["RDS", "Databases", "aws_rds_db_total"],
+    ["S3", "Buckets", "aws_s3_bucket_total"],
+    ["SSM", "Parameters", "aws_ssm_parameter_total"],
+    ["Lambda", "Functions", "aws_lambda_function_total"],
+    ["ECS", "Clusters", "aws_ecs_cluster_total"],
+    ["EC2", "Fleet", "aws_ec2_fleet_total"],
+    ["IAM", "Policies", "aws_iam_policy_total"],
+    ["Dynamo", "Tables", "aws_dynamo_table_total"]
+  ];
   let noteworthyCounts = new Map();
+  let noteworthySubtitles = new Map();
   
-  for (let interest of interests) {
-    var interestCount = metricsWrapper.getAwsResourceCountsForCapabilityAndType(capabilityId, interest);
+  for (let [title, subtitle, resourceName] of interests) {
+    var interestCount = metricsWrapper.getAwsResourceCountsForCapabilityAndType(capabilityId, resourceName);
     if (interestCount > 0) {
-      noteworthyCounts.set(interest, interestCount);
+      noteworthyCounts.set(title, interestCount);
+      noteworthySubtitles.set(title, subtitle)
     }
   }
   let noteworthyInterests = [...noteworthyCounts.keys()];
@@ -55,9 +66,9 @@ export function DetailedAwsCountSummary({ capabilityId }) {
           <p className={styles.resourceHint}>Please note that cards are only shown for resource types in actual use and includes everything for that type according to AWS. Click on any card for the complete list of resources.</p>
           <div className={styles.FlexContainer}  onClick={() => setShowResourcesWindow(true)} >
             {noteworthyInterests.map((i) => (
-              <AwsCountCard key={i} title={i} count={noteworthyCounts.get(i)}/>
+              <AwsCountCard key={i} title={i} subtitle={noteworthySubtitles.get(i)} count={noteworthyCounts.get(i)}/>
             ))}
-            <AwsCountCard title="Total" count={count}/>
+            <AwsCountCard title="Total" subtitle="Everything" count={count}/>
           </div>
         </>
       )}
@@ -65,11 +76,13 @@ export function DetailedAwsCountSummary({ capabilityId }) {
   );
 }
 
-function AwsCountCard({ title, count }) {
+function AwsCountCard({ title, subtitle, count }) {
   return (
     <>
       <div className={styles.SingleCountCard}>
         <span>{title}</span>
+        <br />
+        <span className={styles.SingleCountCardSubtitle}>{subtitle}</span>
         <br />
         <span className={styles.CountNumber}>{count}</span>
       </div>
