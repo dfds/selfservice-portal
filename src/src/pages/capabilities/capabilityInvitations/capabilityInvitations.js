2e1ef840dae3342e8f735937cbf7fee90ea812e8
HrisyToteva <hristiqnatoteva9718@gmail.com>
2023-11-30 13:39:08 +0100

    user feedback after invitations submition

--- src/src/pages/capabilities/capabilityInvitations/capabilityInvitations.js
+++ src/src/pages/capabilities/capabilityInvitations/capabilityInvitations.js
@@ -1,15 +1,27 @@
 import PageSection from "components/PageSection";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button, ButtonStack } from "@dfds-ui/react-components";
 import { Invitations } from "../invitations";
 
 export function CapabilityInvitations({ addNewInvitees, inProgress }) {
   const [invitees, setInvitees] = useState([]);
-
+  const [isInvited, setIsInvited] = useState(false);
+  const [showDoneLabel, setShowDoneLabel] = useState(false);
+ 
   const handleAddInvitationClicked = async () => {
-    await addNewInvitees(invitees);
+    await addNewInvitees(invitees)
     setInvitees([]);
+    setIsInvited(true);
+    setShowDoneLabel(true);
   };
+
+  useEffect(() => {
+    if (isInvited) {
+      setTimeout(() => {
+        setShowDoneLabel(false);
+      }, 3000);
+    }
+  }, [isInvited, showDoneLabel])
 
   return (
     <>
@@ -18,6 +30,7 @@ export function CapabilityInvitations({ addNewInvitees, inProgress }) {
           addNewInvitees={addNewInvitees}
           invitees={invitees}
           setInvitees={setInvitees}
+          isInvited={isInvited}
         />
         <ButtonStack align="right">
           <Button
@@ -25,9 +38,10 @@ export function CapabilityInvitations({ addNewInvitees, inProgress }) {
             variation="primary"
             onClick={handleAddInvitationClicked}
             submitting={inProgress}
-            style={{ position: "right" }}
+            style={{ position: "right",
+            backgroundColor: showDoneLabel ? "#4caf50" : "#ED8800" }}
           >
-            Invite
+            {showDoneLabel ? "Success" : "Invite"}
           </Button>
         </ButtonStack>
       </PageSection>
