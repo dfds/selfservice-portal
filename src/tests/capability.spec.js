b24cd6642ea4105fd68b0a373f08c0b884436780
Emil H. Clausen <sequoiia@hummel.yt>
2023-12-03 22:30:00 +0100

    Added more tests for capability page

--- src/tests/capability.spec.js
+++ src/tests/capability.spec.js
@@ -27,3 +27,95 @@ test("join-capability-ce-force", async ({ page }) => {
     timeout: 1000,
   });
 });
+
+test("leave-capability", async ({ page }) => {
+  await page.goto(
+    "http://localhost:3001/capabilities/marketing-department-xxx",
+  );
+  await page.getByRole("button", { name: "Join" }).click();
+  await page.getByRole("button", { name: "FORCE JOIN (CE)" }).click();
+
+  await page.waitForTimeout(1000);
+  await page.goto(
+    "http://localhost:3001/capabilities/marketing-department-xxx",
+  );
+  await page.getByRole("button", { name: "Leave" }).click();
+  await page.getByRole("dialog").getByRole("button", { name: "Leave" }).click();
+  await page.waitForTimeout(1000);
+  await page.goto(
+    "http://localhost:3001/capabilities/marketing-department-xxx",
+  );
+
+  await expect(page.getByRole("button", { name: "Join" })).toHaveCount(0, {
+    timeout: 1000,
+  });
+});
+
+test("invite-member", async ({ page }) => {
+  await page.goto("http://localhost:3001/capabilities/cloudengineering-xxx");
+  await page.getByPlaceholder("Enter user name").click();
+  await page.getByPlaceholder("Enter user name").fill("Richard f");
+  await page.getByText("Richard Fisher rifis@dfds.com").click();
+  await page.locator("_react=Invitations").getByText("rifis").isVisible();
+  await page.getByRole("button", { name: "Invite" }).click();
+
+  await expect(page.getByRole("button", { name: "Success" })).toBeVisible();
+  await expect(
+    page.locator("_react=Invitations").getByText("rifis"),
+  ).toHaveCount(0, {
+    timeout: 1000,
+  });
+
+  await page.waitForTimeout(500);
+});
+
+test("approve-membership", async ({ page }) => {
+  await page.goto("http://localhost:3001/capabilities/cloudengineering-xxx");
+  await page
+    .getByRole("row", { name: "emwee@dfds.com emwee@dfds.com" })
+    .getByRole("button")
+    .click();
+  await page.getByTitle("You have already submitted").isVisible();
+  await page.getByTitle("You have already submitted").isDisabled();
+
+  await page.goto("http://localhost:3001/capabilities/cloudengineering-xxx");
+
+  await expect(
+    page
+      .getByRole("row", { name: "emwee@dfds.com emwee@dfds.com" })
+      .getByRole("button"),
+  ).toHaveCount(0, {
+    timeout: 1000,
+  });
+
+  await page.waitForTimeout(500);
+});
+
+test("delete-capability", async ({ page }) => {
+  await page.goto("http://localhost:3001/capabilities/notmycapability-xx2");
+  await page.getByRole("button", { name: "Join" }).click();
+  await page.getByRole("button", { name: "FORCE JOIN (CE)" }).click();
+  await page.getByRole("button", { name: "Delete Capability" }).click();
+  await page.getByRole("button", { name: "Delete", exact: true }).click();
+
+  await page.getByText("Warning: Pending Deletion").isVisible();
+
+  await page.waitForTimeout(500);
+});
+
+test("cancel-capability-deletion", async ({ page }) => {
+  await page.goto("http://localhost:3001/capabilities/notmycapability-xx3");
+  await page.getByRole("button", { name: "Join" }).click();
+  await page.getByRole("button", { name: "FORCE JOIN (CE)" }).click();
+  await page.getByRole("button", { name: "Delete Capability" }).click();
+  await page.getByRole("button", { name: "Delete", exact: true }).click();
+
+  await page.getByText("Warning: Pending Deletion").isVisible();
+
+  await page.getByRole("button", { name: "Cancel Deletion" }).click();
+  await page.getByRole("button", { name: "Delete Capability" }).isVisible();
+
+  await page.waitForTimeout(500);
+});
+
+// getByTitle('You have already submitted')
