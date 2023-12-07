78d6a43a5cfcd143f9e41874c49011a62df2fd27
Emil H. Clausen <sequoiia@hummel.yt>
2023-12-07 11:54:00 +0100

    Added fallback in case tool api is not available

--- src/tests/base.js
+++ src/tests/base.js
@@ -3,31 +3,54 @@ import { Mutex } from "async-mutex";
 // @ts-check
 const { test: base, expect } = require("@playwright/test");
 
+const baseUrl = "http://localhost:8045";
+
 function delay(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
+async function isToolApiAvailable(request) {
+  try {
+    await request.get(`${baseUrl}/xaxa`);
+  } catch (error) {
+    console.log(
+      "Tool API unavailable. Falling back to no database reset and no mutex",
+    );
+    return false;
+  }
+
+  console.log("Tool API available. Enabling database reset and mutex");
+  return true;
+}
+
 export const test = base.extend({
-  resetDb: async ({ request }, use) => {
+  resetDb: async ({ request, page }, use) => {
+    let toolAvailable = await isToolApiAvailable(request);
+    if (!toolAvailable) {
+      await use(null);
+      return;
+    }
+
     let retry = true;
     do {
       try {
-        let resp = await request.get("http://localhost:8045/test/lock");
+        let resp = await request.get(`${baseUrl}/test/lock`);
         if (resp.status() === 200) {
           retry = false;
         } else {
           await delay(1000);
         }
-      } catch {
+      } catch (error) {
+        // console.log(error.message.includes("ECONNREFUSED"));
         retry = true;
         await delay(1000);
       }
     } while (retry);
 
-    await request.get("http://localhost:8045/db/reset");
+    await request.get(`${baseUrl}/db/reset`);
     await use(null);
-    await request.get("http://localhost:8045/db/reset");
-    await request.get("http://localhost:8045/test/unlock");
+    await request.get(`${baseUrl}/db/reset`);
+    await request.get(`${baseUrl}/test/unlock`);
   },
 });
 
