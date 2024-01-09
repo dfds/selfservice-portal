81cbe613297c256c5aa087bae7e5aeec53287df2
HrisyToteva <hristiqnatoteva9718@gmail.com>
2024-01-09 10:51:07 +0100

    formatting

--- src/src/pages/capabilities/summary/index.js
+++ src/src/pages/capabilities/summary/index.js
@@ -11,7 +11,6 @@ import { useState } from "react";
 import { MyMembershipApplication } from "../membershipapplications";
 import AppContext from "AppContext";
 
-
 function JoinDialog({
   name,
   isSubmitting,
