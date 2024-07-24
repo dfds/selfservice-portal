d6c23e2a86b5f67513dae470380039d32b48b0e9
Andreas Frisch <andfris@dfds.com>
2024-07-24 11:24:42 +0200

    format

--- src/src/pages/capabilities/capabilityClaims/index.js
+++ src/src/pages/capabilities/capabilityClaims/index.js
@@ -2,11 +2,7 @@ import React, { useContext, useEffect } from "react";
 import PageSection from "components/PageSection";
 import styles from "./capabilityClaims.module.css";
 import { StatusSuccess } from "@dfds-ui/icons/system";
-import {
-  Card,
-  CardContent,
-  Button,
-} from "@dfds-ui/react-components";
+import { Card, CardContent, Button } from "@dfds-ui/react-components";
 import SelectedCapabilityContext from "../SelectedCapabilityContext";
 import { useState } from "react";
 import { useSelfServiceRequest } from "../../../hooks/SelfServiceApi";
@@ -14,8 +10,19 @@ import { useSelfServiceRequest } from "../../../hooks/SelfServiceApi";
 function ClaimRow({ description, link, claimedAt, claimFunction }) {
   return (
     <div className={styles.claimRow}>
-      {claimedAt && <div><StatusSuccess className={styles.claimedIcon}/></div>}
-      {!claimedAt && <Button className={styles.claimButton} onClick={() => claimFunction(link)}>Claim</Button>}
+      {claimedAt && (
+        <div>
+          <StatusSuccess className={styles.claimedIcon} />
+        </div>
+      )}
+      {!claimedAt && (
+        <Button
+          className={styles.claimButton}
+          onClick={() => claimFunction(link)}
+        >
+          Claim
+        </Button>
+      )}
       <div>{description}</div>
     </div>
   );
@@ -23,13 +30,12 @@ function ClaimRow({ description, link, claimedAt, claimFunction }) {
 
 export default function CapabilityClaims() {
   const { responseData, sendRequest } = useSelfServiceRequest();
-  const { inProgress: claimInProgress, sendRequest: addClaim } = useSelfServiceRequest();
+  const { inProgress: claimInProgress, sendRequest: addClaim } =
+    useSelfServiceRequest();
   const [claims, setClaims] = useState([]);
   const [reloadClaims, setReloadClaims] = useState(true);
   const [showClaimsSection, setShowClaimsSection] = useState(false);
-  const {
-    links
-  } = useContext(SelectedCapabilityContext);
+  const { links } = useContext(SelectedCapabilityContext);
 
   const sendClaimRequest = (link) => {
     if (link?.href) {
@@ -60,7 +66,7 @@ export default function CapabilityClaims() {
       setReloadClaims(false);
     }
   }, [reloadClaims]);
-  
+
   useEffect(() => {
     if (claims.length > 0) {
       setShowClaimsSection(true);
@@ -69,17 +75,20 @@ export default function CapabilityClaims() {
 
   return (
     <>
-    {showClaimsSection && (
-      <PageSection headline="Capability Claims">
-        <Card>
-          <CardContent>
-            <p>
-              Some external dependencies and integrations for capabilities are monitored automatically and others are not.
-              In order to still indicate such integrations or even just adherance to company rules, capability owners can say that they live up to these standards.
-              Thus, a Capability Claim is nothing more than capability members indicating (claiming) such adherance.
-            </p>
+      {showClaimsSection && (
+        <PageSection headline="Capability Claims">
+          <Card>
+            <CardContent>
+              <p>
+                Some external dependencies and integrations for capabilities are
+                monitored automatically and others are not. In order to still
+                indicate such integrations or even just adherance to company
+                rules, capability owners can say that they live up to these
+                standards. Thus, a Capability Claim is nothing more than
+                capability members indicating (claiming) such adherance.
+              </p>
 
-            {(claims || []).map((claim) => (
+              {(claims || []).map((claim) => (
                 <ClaimRow
                   key={claim.claim}
                   description={claim.claimDescription}
@@ -88,10 +97,10 @@ export default function CapabilityClaims() {
                   claimFunction={sendClaimRequest}
                 ></ClaimRow>
               ))}
-          </CardContent>
-        </Card>
-      </PageSection>
-    )}
+            </CardContent>
+          </Card>
+        </PageSection>
+      )}
     </>
   );
 }
