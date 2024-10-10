e06fc5fec4f7e94d4f50dbc6b021336d7ea5f4f3
Emil H. Clausen <sequoiia@hummel.yt>
2024-10-10 09:10:38 +0200

    chore: linter

--- src/src/state/local/auth.ts
+++ src/src/state/local/auth.ts
@@ -69,6 +69,7 @@ export const auth: Slice<AuthStruct> = createSlice({
   },
 });
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function isTokenExpired(token: string): boolean {
   const decoded = jwtDecode(token);
   const currentTime = Math.floor(Date.now() / 1000);
