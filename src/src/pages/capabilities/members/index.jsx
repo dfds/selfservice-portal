7af72735eff792bb4857f51715adc74770b0ee9e
Andreas Frisch <andfris@dfds.com>
2024-11-19 13:23:18 +0100

    Reorder details page

--- src/src/pages/capabilities/members/index.jsx
+++ src/src/pages/capabilities/members/index.jsx
@@ -2,7 +2,10 @@ import React, { useContext } from "react";
 import styles from "./members.module.css";
 import { Text } from "@dfds-ui/typography";
 import ProfilePicture from "./profilepicture";
+import { TabbedPageSection } from "../../../components/PageSection";
 import SelectedCapabilityContext from "../SelectedCapabilityContext";
+import MembershipApplications from "../membershipapplications";
+import { CapabilityInvitations } from "../capabilityInvitations/capabilityInvitations";
 
 export default function Members() {
   const { members } = useContext(SelectedCapabilityContext);
@@ -20,5 +23,40 @@ export default function Members() {
         ))}
       </div>
     </>
+  );
+}
+
+export function TabbedMembersView({ showInvitations }) {
+  const header = <></>;
+  const footer = <></>;
+
+  const tabs = {
+    members: "Current Members",
+    invitations: "Invite Members",
+    applications: "Membership Applications",
+  };
+
+  const tabsContent = {
+    members: <Members />,
+    invitations: (
+      <>
+        {showInvitations ? (
+          <CapabilityInvitations />
+        ) : (
+          <Text>You do not have access to view this information.</Text>
+        )}
+      </>
+    ),
+    applications: <MembershipApplications />,
+  };
+
+  return (
+    <TabbedPageSection
+      headline="Capability Member Management"
+      tabs={tabs}
+      tabsContent={tabsContent}
+      header={header}
+      footer={footer}
+    />
   );
 }
