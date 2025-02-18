b66a69dd7c439ab5b18267e3f224da0bcc8eec31
Andreas Frisch <andfris@dfds.com>
2025-02-18 14:58:48 +0100

    add link to azure ai guide to frontpage

--- src/src/pages/frontpage/QuickLinks.jsx
+++ src/src/pages/frontpage/QuickLinks.jsx
@@ -34,6 +34,7 @@ export default function QuickLinks() {
       />
       <Link title={"Swagger API Documentation"} url={swaggerUrl} />
       <Link title={"Chat Bot assistance"} url={chatUrl} />
+      <Link title={"AI Deployment in Azure"} url="https://wiki.dfds.cloud/en/playbooks/ai-deployment-azure/getting-started" />
     </div>
   );
 }
