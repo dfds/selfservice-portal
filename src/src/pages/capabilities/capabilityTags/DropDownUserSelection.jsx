a1a39c5d01e2e3136e03f9fa99f4122a65bf13a2
Andreas Frisch <andfris@dfds.com>
2025-03-31 09:27:28 +0200

    Control errors and error visuality clearly

---
+++ src/src/pages/capabilities/capabilityTags/DropDownUserSelection.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import style from "./dropdownmenu.module.css";
+
+export default function DropDownUserSelection({
+  items,
+  addUserFromDropDown,
+}) {
+
+  const handleUserClicked = (user) => {
+    addUserFromDropDown(user.email);
+  };
+
+  return (
+    <>
+      <div>
+        <div className={style.items}>
+          {(items || []).map((item) => (
+            <div
+              key={item.id}
+              className={style.item}
+              onClick={() =>
+                handleUserClicked(item)
+              }
+            >
+              {item.name}{" ("}{item.email}{")"}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
