8a679019344d7ed1044ffe13aabbc645b2e6290d
Andreas Frisch <andfris@dfds.com>
2025-08-05 09:50:02 +0200

    format

--- src/src/components/ConsumerLink/index.jsx
+++ src/src/components/ConsumerLink/index.jsx
@@ -6,7 +6,11 @@ export default function ConsumerLink({ capabilityId, topicName, linkTitle }) {
     <TrackedButton
       trackName="ConsumerLink"
       onClick={() =>
-       window.open(`https://view.grafana.dfds.cloud/d/mihl5sn/topic-consumers?orgId=1&from=now-30d&to=now&timezone=browser&var-prefix=.%2A&var-namespace=${capabilityId}&var-topic=${topicName}`, '_blank', 'noopener,noreferrer')
+        window.open(
+          `https://view.grafana.dfds.cloud/d/mihl5sn/topic-consumers?orgId=1&from=now-30d&to=now&timezone=browser&var-prefix=.%2A&var-namespace=${capabilityId}&var-topic=${topicName}`,
+          "_blank",
+          "noopener,noreferrer",
+        )
       }
     >
       {linkTitle}
