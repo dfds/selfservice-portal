9277e87912c136d2e597305eba2433268a15d1a9
Andreas Frisch <andfris@dfds.com>
2025-12-04 13:31:06 +0100

    even shorter notice

--- src/src/pages/GrafanaWarning.jsx
+++ src/src/pages/GrafanaWarning.jsx
@@ -7,26 +7,16 @@ export default function GrafanaWarning() {
       <Text style={headerStyle}>
         Urgent: Grafana Metrics Cost Reduction Requirement
       </Text>
+      <Text>Call for action: Update your environment tag!</Text>
       <Text>
-        We are close to running out of Grafana credits due to the current high
-        volume of metrics being ingested
-      </Text>
-      <Text>
-        If this continues, we risk losing critical observability insights.
-      </Text>
-      <Text>
-        We need <strong>you</strong> to take immediate action to reduce
-        unnecessary metrics.
-      </Text>
-      <Text>
-        Please read{" "}
+        Read more{" "}
         <TrackedLink
           trackName="Wiki-Grafana-Tracking-Issue"
           href="https://wiki.dfds.cloud/en/announcements/grafana-cost-reduction"
         >
-          these instructions
-        </TrackedLink>{" "}
-        for more information.
+          here
+        </TrackedLink>
+        .
       </Text>
     </div>
   );
