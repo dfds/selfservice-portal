e74937ee3b93be5dcd52ccae8ad69622571421dc
Emil H. Clausen <sequoiia@hummel.yt>
2026-09-07 14:41:07 +0200

    disable matomo

--- src/src/TrackingContext.jsx
+++ src/src/TrackingContext.jsx
@@ -12,14 +12,14 @@ function TrackingProvider({ children }) {
 
   useEffect(() => {
     if (isEnabled) {
-      var _mtm = (window._mtm = window._mtm || []);
-      _mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" });
-      var d = document,
-        g = d.createElement("script"),
-        s = d.getElementsByTagName("script")[0];
-      g.async = true;
-      g.src = "https://build.dfds.cloud/tr/js/container_nbYsx3GM.js";
-      s.parentNode.insertBefore(g, s);
+      // var _mtm = (window._mtm = window._mtm || []);
+      // _mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" });
+      // var d = document,
+      //   g = d.createElement("script"),
+      //   s = d.getElementsByTagName("script")[0];
+      // g.async = true;
+      // g.src = "https://build.dfds.cloud/tr/js/container_nbYsx3GM.js";
+      // s.parentNode.insertBefore(g, s);
     }
   }, []);
 
--- src/src/hooks/Tracking.js
+++ src/src/hooks/Tracking.js
@@ -12,7 +12,7 @@ export function useTracking(opts) {
   let track = (...opts) => {
     if (trackingIsEnabled) {
       // eslint-disable-next-line no-undef
-      _paq.push(["trackEvent", ...opts]);
+      // _paq.push(["trackEvent", ...opts]);
     }
     if (swetrixIsEnabled) {
       const [category, label, ...rest] = opts;
